8239877403da372f3aeed6f8d6df777c6ac2c1cf
Salman Shaikh <coder88salman@gmail.com>
2023-11-02 23:01:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const submitBtn = document.getElementById("submitBtn");
 function formatDate(date) {
   const months = [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
   ];
-  
+
   const day = date.getDate();
   const month = months[date.getMonth()];
   const year = date.getFullYear();
@@ -12,26 +22,21 @@ function formatDate(date) {
   return `${month} ${day}, ${year}`;
 }
 
-
-
-
 const currentDate = new Date();
 const formattedDate = formatDate(currentDate);
 const { PDFDocument, rgb, degrees } = PDFLib;
 
 const generatePDF = async (name, skill_boost_url) => {
+  // Use a regular expression to match the ID pattern in the URL
+  const idMatch = skill_boost_url.match(/\/public_profiles\/([\w-]+)$/);
 
-
-// Use a regular expression to match the ID pattern in the URL
-const idMatch = skill_boost_url.match(/\/public_profiles\/([\w-]+)$/);
-
-let id;
-if (idMatch) {
- id = idMatch[1];
-  console.log(id); // This will log the ID: "c5b5d61d-8094-426f-a43c-172a9c704e00"
-} else {
-  console.log('ID not found in the URL.');
-}
+  let id;
+  if (idMatch) {
+    id = idMatch[1];
+    console.log(id); // This will log the ID: "c5b5d61d-8094-426f-a43c-172a9c704e00"
+  } else {
+    console.log("ID not found in the URL.");
+  }
 
   const existingPdfBytes = await fetch("../certificate_temp.pdf").then((res) =>
     res.arrayBuffer()
@@ -42,8 +47,8 @@ if (idMatch) {
   pdfDoc.registerFontkit(fontkit);
 
   // Get the custom font
-  const fontBytes = await fetch("./GoogleSansDisplay-Regular-v1.27.ttf").then((res) =>
-    res.arrayBuffer()
+  const fontBytes = await fetch("./GoogleSansDisplay-Regular-v1.27.ttf").then(
+    (res) => res.arrayBuffer()
   );
 
   // Embed the custom font in the document
@@ -76,21 +81,19 @@ if (idMatch) {
   const gradeX = (pageWidth - nameTextWidth) / 2;
   const gradeY = nameY - 30;
   const gradeText = formattedDate;
-  
+
   firstPage.drawText(gradeText, {
     x: 254.87,
     y: 477,
     size: gradeFontSize,
     font: SanChezFont,
     color: rgb(140 / 255, 143 / 255, 148 / 255), // Set color to #8C8F94
-
   });
-
 
   // Draw the grade
   const urlFontSize = 8.05;
   const urlText = `gdscpescoe.tech/studyjam/verify/${id}`;
-  const urlCodeX = pageWidth  - 150; // Adjust the position
+  const urlCodeX = pageWidth - 150; // Adjust the position
   const urlCodeY = 40; // Adjust the position
 
   firstPage.drawText(urlText, {
@@ -98,19 +101,21 @@ if (idMatch) {
     y: urlCodeY,
     size: urlFontSize,
     font: SanChezFont,
-    width : 10,
+    width: 10,
     color: rgb(103 / 255, 108 / 255, 114 / 255), // Set color to RGB(103, 108, 114)
   });
-    // Calculate the position and size for the URL text
-   
-  
+  // Calculate the position and size for the URL text
 
   // Fetch the QR code image
-  const qrCodeResponse = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://gdscpescoe.tech/studyjam/verify/${id}`);
+  const qrCodeResponse = await fetch(
+    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://gdscpescoe.tech/studyjam/verify/${id}`
+  );
   const qrCodeImageBlob = await qrCodeResponse.blob();
 
   // Convert the blob to a Uint8Array
-  const qrCodeImageData = new Uint8Array(await new Response(qrCodeImageBlob).arrayBuffer());
+  const qrCodeImageData = new Uint8Array(
+    await new Response(qrCodeImageBlob).arrayBuffer()
+  );
 
   // Embed the QR code image on the PDF
   const qrCodeImage = await pdfDoc.embedPng(qrCodeImageData);
@@ -129,12 +134,8 @@ if (idMatch) {
   const pdfBytes = await pdfDoc.save();
 
   // Create and download the PDF file
-  var file = new File(
-    [pdfBytes],
-    `${name}_Certificate.pdf`,
-    {
-      type: "application/pdf;charset=utf-8",
-    }
-  );
+  var file = new File([pdfBytes], `${name}_Certificate.pdf`, {
+    type: "application/pdf;charset=utf-8",
+  });
   saveAs(file);
 };
